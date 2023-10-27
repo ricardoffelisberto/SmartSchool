1a523dc9176cc69a9fd8d3640e777bc2e85b9954
ricardoffelisberto <ricardoffelisberto@gmail.com>
2023-10-26 23:29:47 -0300

    criacao de contexto sqlite

---
+++ SmartSchoolAPI/Data/SmartContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using SmartSchoolAPI.Models;
+
+namespace SmartSchoolAPI.Data
+{
+    public class SmartContext : DbContext
+    {
+        public SmartContext(DbContextOptions<SmartContext> options) : base(options) { }
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Professor> Professores { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+        public DbSet<AlunoDisciplina> AlunosDisciplinas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AlunoDisciplina>()
+                .HasKey(AD => new{AD.AlunoId, AD.DisciplinaId});
+        }
+    }
+}
